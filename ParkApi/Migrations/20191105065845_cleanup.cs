8aed8a62aca81523810a0d83a56ec64e8c56a87a
Hailey Gaylor <haileygaylor@gmail.com>
2019-11-04 23:02:19 -0800

    Made nswag update to startup

---
+++ ParkApi/Migrations/20191105065845_cleanup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkApi.Migrations
+{
+    public partial class cleanup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1,
+                column: "Location",
+                value: "South Cascades, Mount St. Helens");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1,
+                column: "Location",
+                value: "South Cascases, Mount St. Helens");
+        }
+    }
+}
